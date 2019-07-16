7249301c19749f8bb1c48071b55c56e521253ffe
robinsontom <robinsontom@google.com>
2019-07-16 12:56:27 -0700

    Add test for ListView.builder

--- packages/flutter/test/widgets/list_view_visible_items_test.dart
+++ packages/flutter/test/widgets/list_view_visible_items_test.dart
@@ -63,4 +63,29 @@ void main() {
     expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 2, itemLeadingEdge: 0.2, itemTrailingEdge: 0.5)));
     expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 3, itemLeadingEdge: 0.5, itemTrailingEdge: 0.9)));
   });
+
+  testWidgets('List positions of builder list', (WidgetTester tester) async {
+    tester.binding.window.physicalSizeTestValue = const Size(200.0, 200.0);
+    tester.binding.window.devicePixelRatioTestValue = 1.0;
+
+    final ItemPositionNotifier itemScrollController = ItemPositionNotifier();
+
+    await tester.pumpWidget(
+      Directionality(
+        textDirection: TextDirection.ltr,
+        child: ListView.builder(
+          itemCount: 15,
+          itemBuilder: (BuildContext context, int i) => SizedBox(
+            height: (i + 1) * 20.0,
+            child: Text('Item $i'),
+          ),
+          itemScrollController: itemScrollController,
+        ),
+      ),
+    );
+
+    expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 0, itemLeadingEdge: 0, itemTrailingEdge: 0.1)));
+    expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 1, itemLeadingEdge: 0.1, itemTrailingEdge: 0.3)));
+    expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 2, itemLeadingEdge: 0.3, itemTrailingEdge: 0.6)));
+  });
 }
