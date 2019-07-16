a903c1dd70d912ed006c101f6780cd776b7e7767
robinsontom <robinsontom@google.com>
2019-07-16 09:48:24 -0700

    change size of items in test

--- packages/flutter/test/widgets/list_view_visible_items_test.dart
+++ packages/flutter/test/widgets/list_view_visible_items_test.dart
@@ -20,7 +20,7 @@ void main() {
           children: List<Widget>.generate(
             15,
             (int i) => SizedBox(
-              height: (i + 1) * 10.0,
+              height: (i + 1) * 20.0,
               child: Text('Item $i'),
             ),
           ),
@@ -32,8 +32,8 @@ void main() {
     expect(find.text('Item 0'), findsOneWidget);
     expect(find.text('Item 10'), findsNothing);
 
-    expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 0, itemLeadingEdge: 0, itemTrailingEdge: 0.05)));
-    expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 1, itemLeadingEdge: 0.05, itemTrailingEdge: 0.15)));
-    expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 2, itemLeadingEdge: 0.15, itemTrailingEdge: 0.3)));
+    expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 0, itemLeadingEdge: 0, itemTrailingEdge: 0.1)));
+    expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 1, itemLeadingEdge: 0.1, itemTrailingEdge: 0.3)));
+    expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 2, itemLeadingEdge: 0.3, itemTrailingEdge: 0.6)));
   });
 }
