92b150249fc03fdc54d1471b8b9b2a3092921028
robinsontom <robinsontom@google.com>
2019-07-15 20:07:55 -0700

    list visible items in varying size list

--- packages/flutter/test/widgets/list_view_visible_items_test.dart
+++ packages/flutter/test/widgets/list_view_visible_items_test.dart
@@ -3,12 +3,15 @@
 // found in the LICENSE file.
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
   testWidgets('init', (WidgetTester tester) async {
     tester.binding.window.physicalSizeTestValue = const Size(200.0, 200.0);
     tester.binding.window.devicePixelRatioTestValue = 1.0;
+
+    final ItemScrollController itemScrollController = ItemScrollController();
 
     await tester.pumpWidget(
       Directionality(
@@ -17,15 +20,20 @@ void main() {
           children: List<Widget>.generate(
             15,
             (int i) => SizedBox(
-              height: 20.0,
+              height: (i + 1) * 10.0,
               child: Text('Item $i'),
             ),
           ),
+        itemScrollController: itemScrollController,
         ),
       ),
     );
 
     expect(find.text('Item 0'), findsOneWidget);
     expect(find.text('Item 10'), findsNothing);
+
+    expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 0, itemLeadingEdge: 0, itemTrailingEdge: 0.05)));
+    expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 1, itemLeadingEdge: 0.05, itemTrailingEdge: 0.15)));
+    expect(itemScrollController.itemPositions.value, contains(SliverChildPosition(index: 2, itemLeadingEdge: 0.15, itemTrailingEdge: 0.3)));
   });
 }
